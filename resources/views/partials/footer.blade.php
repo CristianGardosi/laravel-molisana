124d11ea98f5635af9408e10e1e3caff1ce606ba
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-12-24 10:37:52 +0100

    FOOTER CREATION/STYLING

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,113 @@
+<footer>
+    <div class="container footer">
+        <div class="footer-logo-contacts">
+            <a href="{{ route('home') }}">
+                <img src="{{asset('img/logo.png')}}" alt="Logo">
+           </a>
+           <small>
+            Ragione sociale: La Molisana S.P.A.
+            Sede legale: Contrada Colle delle Api, 100/A - 86100 - Campobasso (CB)
+            Pec: lamolisana@pec.it
+            Tel: +39 0874 4981
+            Fax: +39 0874 629584
+            Per segnalazioni scrivere a:
+            info@lamolisana.it
+            Per contattare l'area commerciale:
+            commerciale@lamolisana.it
+            Per contattare l'area export:
+            export@lamolisana.it
+            Dati aziendali: leggi di più
+           </small>
+        </div>
+        <div class="pastificio links-collection">
+            <h3>Il Pastificio</h3>
+            <ul class="links-list">
+                <li>
+                    <a href="#">
+                        Il pastificio
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Grano decorticato a pietra
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Il Molise c'è
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Filiera integrata
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        100 anni di pasta
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="prodotti links-collection">
+            <h3>Prodotti</h3>
+            <ul class="links-list">
+                <li>
+                    <a href="#">
+                        Il pastificio
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Grano decorticato a pietra
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Il Molise c'è
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Filiera integrata
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        100 anni di pasta
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="collezione links-collection">
+            <h3>Collezione da Chef</h3>
+            <ul class="links-list">
+                <li>
+                    <a href="#">
+                        Il pastificio
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Grano decorticato a pietra
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Il Molise c'è
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Filiera integrata
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        100 anni di pasta
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
