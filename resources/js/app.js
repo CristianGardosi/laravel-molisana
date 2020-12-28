d694542b33068a58c0b6f1b8527b252e67dd6638
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-12-28 11:45:49 +0100

    WORKING HAMBURGER MENU FOR TABLET AND MOBILE

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 require('./bootstrap');
+// !Import VUEJS & AXIOS
+import Vue from 'vue';
+import axios from 'axios';
+
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-list');
+const linkss = document.querySelectorAll('.nav-item');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open')
+});
+
+
+
+// * Active class for the selected nav-link * \\
+let current = window.location.href;
+let links = document.querySelectorAll('.nav-link');
+
+
+for (let i = 0; i < links.length; i++) {
+    if(links[i] == current) {
+        links[i].classList.add = "active";
+    }
+}
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,30 +5,37 @@
          <img src="{{asset('img/logo.png')}}" alt="Logo">
     </a>
     <nav class="navbar">
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('home') }}">
-                <h3>Home</h3>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="http://127.0.0.1:8000/product/0">
-                <h3>Prodotti</h3>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('ricette') }}">
-                <h3>Ricette</h3>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('news') }}">
-                <h3>News</h3>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('storia') }}">
-                <h3>La nostra storia</h3>
-            </a>
-        </li>
+        <div class="hamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+        <ul class="nav-list">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('home') }}">
+                    <h3>Home</h3>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://127.0.0.1:8000/product/0">
+                    <h3>Prodotti</h3>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('ricette') }}">
+                    <h3>Ricette</h3>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('news') }}">
+                    <h3>News</h3>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('storia') }}">
+                    <h3>La nostra storia</h3>
+                </a>
+            </li>
+        </ul>
     </nav>
 </header>
